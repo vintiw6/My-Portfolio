808ecbd60bb4b28c489d4a735fc74aeac42c6cc2
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-04-07 19:39:14 +0530

    changed things

---
+++ src/components/Themetoggle.jsx
@@ -0,0 +1,8 @@
+<div className="flex justify-end items-center p-4">
+	<button
+		onClick={toggleTheme}
+		className="w-10 h-10 md:w-12 md:h-12 rounded-full bg-gray-300 dark:bg-gray-700 transition-all cursor-pointer"
+	>
+		{/* Your toggle icon */}
+	</button>
+</div>;
