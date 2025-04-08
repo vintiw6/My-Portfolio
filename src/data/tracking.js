a729a9a20ff921b64f8359cc5f1606ab69cd2e66
Vinayak Tiwari <159702509+vintiw6@users.noreply.github.com>
2025-04-08 23:16:02 +0530

    updated tracking.js

--- src/data/tracking.js
+++ src/data/tracking.js
@@ -1,13 +1,3 @@
-// src/data/tracking.js
-
-// Example: Tracking functions (you can extend this if needed)
-
-// Logs a simple page view
-export function trackPageView(page) {
-	console.log(`Page view: ${page}`);
-}
-
-// Logs a button click
-export function trackButtonClick(buttonName) {
-	console.log(`Button clicked: ${buttonName}`);
-}
+// Google Analytics Tracking ID
+// https://github.com/truethari/reactfolio#-google-analytics
+export const TRACKING_ID = '';
